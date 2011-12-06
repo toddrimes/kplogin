06c533670288e17ed7f4b57417a28b27efccc4f8
Todd Rimes <todd@rimesmedia.com>
2011-12-06 01:42:54 -0800

    Initial Commit

---
+++ kplogin/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  kplogin
+//
+//  Created by TODD RIMES on 12/6/11.
+//  Copyright (c) 2011 Rimes Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ kplogin/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  kplogin
+//
+//  Created by TODD RIMES on 12/6/11.
+//  Copyright (c) 2011 Rimes Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
